cd30b015026bae1b1c72b171fe8194f111e9f5ec
201815069 <70687333+eunchae01@users.noreply.github.com>
2021-09-05 14:12:52 +0900

    0905

--- src/pages/Leader.js
+++ src/pages/Leader.js
@@ -30,10 +30,10 @@ function Leader(){
                 <div className='modal-title'>내 아이디 등록하기</div>
                 <p className='modalT'>학교 이메일을 입력해주세요.</p>
                 <input className='modalB' placeholder='학교이메일'></input>
-                <button>인증</button>
+                <button className='selectB'>인증</button>
                 <p className='modalT'>인증번호를 입력해주세요.</p>
                 <input className='modalB' placeholder='인증번호'></input>
-                <button>확인</button>
+                <button className='selectB'>확인</button>
                 <div className='close-wraper'>
                     <button id='close' onClick={closeModal}>닫기</button>
                 </div>
